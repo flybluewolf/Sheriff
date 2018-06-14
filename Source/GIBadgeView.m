031798b543cb32482f08e6b19083ccf692133281
Xavier Vicient <xavier.vicient@21buttons.com>
2018-06-14 13:35:11 +0200

    Add tw updates

--- Source/GIBadgeView.m
+++ Source/GIBadgeView.m
@@ -8,8 +8,8 @@
 
 #import "GIBadgeView.h"
 
-static CGFloat const kBadgeViewMinimumSize = 20.0;
-static CGFloat const kBadgeViewPadding = 5.0;
+static CGFloat const kBadgeViewMinimumSize = 14.0;
+static CGFloat const kBadgeViewPadding = 2.0;
 static CGFloat const kBadgeViewDefaultFontSize = 12.0;
 
 @interface GIBadgeView ()
